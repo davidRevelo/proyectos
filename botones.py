f59f6a2894d7fa7e79ae3cad57b6a4274b46f6e6
22B18-PC\22B <david.revelo@epn.edu.ec>
2017-05-17 12:47:48 -0500

    primera version

---
+++ botones.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 17 11:59:41 2017
+
+@author: 22B
+"""
+from tkinter import *
+
+root = Tk()
+boton = Button(root, text="Minimizar")
+boton.pack()
+root.mainloop()
